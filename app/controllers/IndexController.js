a2eb19221bd1c64ea35a62da7b0ef5f24c523955
91ahmed <ahmedh12491@gmail.com>
2020-07-11 16:54:03 +0200

    new updates

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -3,6 +3,7 @@ const Mysql = require('../../lib/database/mysql')
 exports.index = (req, res) =>
 {
 	//res.render('index', {title: 'Home'})
+	new Mysql('users').select('name, id').paginate(req, 3);
 }
 
 exports.send = (req, res) => 
