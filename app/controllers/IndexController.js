b138e68f5711c313f1be98fcfaf8bfe6aca7caa0
91ahmed <ahmedh12491@gmail.com>
2020-07-15 11:04:27 +0200

    add mongodb client

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -1,14 +1,19 @@
-const Mysql = require('../../lib/database/mysql')
+const Mysql      = require('../../lib/database/mysql')
 const PostgreSql = require('../../lib/database/postgresql')
+const Mongo      = require('../../lib/database/mongodb')
 
 exports.index = (req, res) =>
 {
+	new Mongo().database('application')
+		       .collection('admins')
+		       .findSomeFields({ _id: 1, name: 1});
+
 	res.render('index', {title: 'Home'})
 }
 
-exports.send = (req, res) => 
+exports.send = (req, res) =>
 { 
 	var name = req.body.username;
 
-	res.send(name)
+	res.send(name);
 }
